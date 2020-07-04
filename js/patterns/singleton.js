fe46d6add42707f2794d1e8e69b1d247afd39fbf
timurmaio <timurmaio@gmail.com>
2020-07-04 22:22:38 +0300

    WIP

--- js/patterns/singleton.js
+++
@@ -1,8 +0,0 @@
-export let object = {
-    name: 'Timur',
-    counter: 1
-}
-
-export function increment() {
-    object.counter += 1;
-}
--- js/patterns/step.js
+++
@@ -1,6 +0,0 @@
-import { object, increment } from './singleton';
-
-export default function transform() {
-    increment();
-    object.name = 'Bulat'
-}
